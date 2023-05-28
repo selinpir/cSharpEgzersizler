23b5f76dcc7e7bdda227f6637a70dbd4065bc950
selin pir <113732977+selinpir@users.noreply.github.com>
2023-05-28 03:53:24 +0300

    random alıntı

---
+++ randomALİNTİ/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace randomquote
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(178, 516);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Alıntı Değiştir";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(178, 112);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(623, 313);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(956, 611);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Alıntılar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ randomALİNTİ/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace randomquote
+{
+    public partial class Form1 : Form
+    {
+        private Random random;
+        // private RichTextBox alinti;
+
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            richTextBox1.ReadOnly = true;
+
+            string[] alinti = {
+                "Acı bir armağandır. Acı hissimiz olmasaydı, verdiğimiz acıyı hissedemezdik.- Doctor Who\r\n",
+                 "\v Filizlendikten sonra bir fikri, asla öldüremezsiniz.-Sherlock\r\n",
+                  "\v Bu dünyadan özgür bir adam olarak gitmek kadar büyük bir zafer yoktur.- Spartacus\r\n",
+                   "\v İnsanlar size kim olduklarını anlatırlar ama biz inanmayız.\vÇünkü biz onların, olmasını istediğimiz kişiler olmalarını isteriz.- Mad Man\r\n",
+                    "\v Suya dinamit atana kadar balık tutmaya devam edebilirsin.-  The Big Bang Theory\r\n",
+                     "\v Eğer her bir saman tanesi bilgisayara kaydedildiyse, samanlıkta iğne aramak o kadar da zor değildir.- Dexter\r\n",
+                      "\v Neden bizi fark edenleri değil de, fark etmeyenleri isteriz?- Shameless\r\n",
+                       "\v Duygu denen şey, sadece kaybeden tarafta bulunan kimyasal bir kusurdur. Her zaman aşkın, tehlikeli bir dezavantaj olduğunu varsaymışımdır.- Sherlock\r\n",
+                        "\v Nefret etmemek o kadar zor ki… İnsanlardan, eşyalardan, geleneklerden. Senin azmini kırıp kanamanı izlerken, zevkten dört köşe olduklarında, mantıklı gelen tek his nefret oluyor. Ama ben nefretin insanı ne hale getirdiğini biliyorum. Nefret insanı paramparça eder. Olmadığı birisine dönüştürür. Hiçbir zaman dönüşmeyeceğine söz verdiği birisine.Sons of Anarchy\r\n",
+                         "\v Herkes yalnız ölür ama birileri için bir şey ifade ettiysen, birilerine yardım ettiysen ya da birilerini sevdiysen, tek bir kişi bile seni hatırlıyorsa belki de aslında hiç ölmezsin.- Person of Interest\r\n",
+            };
+            int randomIndex = random.Next(0, alinti.Length);
+            string randomalinti = alinti[randomIndex];
+
+            richTextBox1.Text = randomalinti;
+
+        }
+    }
+}
