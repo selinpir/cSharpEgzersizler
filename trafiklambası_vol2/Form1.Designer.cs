415393a26e1d6b9c341c44a1a4907cef18eb4347
selin pir <113732977+selinpir@users.noreply.github.com>
2023-06-09 02:04:37 +0300

    lamba-2

---
+++ trafiklambası_vol2/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(217, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(138, 102);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(217, 170);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(138, 102);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(217, 291);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(138, 102);
+            this.panel3.TabIndex = 2;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(430, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(430, 225);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(430, 345);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(266, 430);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(217, 465);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 53);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "BAS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 549);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Trafik ışığı simülasyonu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ trafiklambası_vol2/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+
+namespace WindowsFormsApp1
+{
+           /*  Trafik ışığı simülasyonu(form)
+
+              * Kırmızı 45 sn yansın
+              * Sarı ile kırmızı birlikte 2 saniye yanıp sönecek
+              * Yeşil yanacak 30 sn.
+              * Yeşil 3 sn yanıp sönecek
+              * Kalan süre de gösterilecek --→ Kırmızı geri sayarken kırmızı ile gösterilecek, yeşil geriye sayarken yeşik ile gösterilecek from üzernde yap
+              */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        int sayac = 0;
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+          //  panel1.BackColor = Color.Red;
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            //panel1.BackColor = Color.Yellow;
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            // panel1.BackColor = Color.Green;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            label4.Text = sayac.ToString();
+            if (sayac >= 0 && sayac < 45)
+            {
+                panel1.BackColor = Color.Red;
+                label1.Text = sayac.ToString();              
+            }
+            if (sayac == 45)
+            {
+                panel1.BackColor = Color.White;
+            }
+
+            if (sayac >= 45 && sayac < 47)
+            {
+                panel1.BackColor = Color.Red;
+                panel2.BackColor = Color.Yellow;
+                label2.Text = sayac.ToString();              
+            }
+            if (sayac == 47)
+            {
+                panel1.BackColor = Color.White;
+                panel2.BackColor = Color.White;
+            }
+
+            if (sayac >= 47 && sayac< 97)
+            {
+                panel3.BackColor = Color.Green;
+                label3.Text = sayac.ToString();
+               
+            }
+
+            if (sayac == 97)
+            { panel3.BackColor = Color.White; }
+
+            if (sayac >= 97 && sayac <= 100)
+            {
+                panel3.BackColor = Color.Green;
+                sayac = 0;
+            }
+            if (sayac == 0)
+            { panel3.BackColor = Color.White; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
