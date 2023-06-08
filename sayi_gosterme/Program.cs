1166fac456e5659d4cf67c8904ff62cf664be6ab
selin pir <113732977+selinpir@users.noreply.github.com>
2023-06-09 02:06:29 +0300

    3-> 1 2 2

---
+++ sayi_gosterme/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace sayi_gosterme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            {
+                Console.Write("Bir sayı girin: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+
+                sayilar(sayi);
+
+                Console.ReadLine();
+            }
+
+        static void sayilar(int sayi)
+            {
+            int sayac = 1;
+            for (int i = 1; i <= sayi; i++)
+                {
+                    for (int j = 0; j < sayac; j++)
+                    {
+                        Console.Write(i + " ");
+                    }
+                    sayac++;
+                }
+            }
+        }
+    }
